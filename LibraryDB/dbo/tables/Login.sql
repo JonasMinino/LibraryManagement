48c513d4f3d1c6228db03ecd7230e1def858286c
jonasminino <jonasminino@gmail.com>
2021-01-12 15:25:45 -0500

    Added testing data to the login table.

--- LibraryDB/dbo/tables/Login.sql
+++ LibraryDB/dbo/tables/Login.sql
@@ -2,6 +2,6 @@
 (
 	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Username] VARCHAR(30) NOT NULL UNIQUE, 
-    [Password] VARBINARY(30) NOT NULL, 
+    [Password] VARCHAR(30) NOT NULL, 
     [Email] VARCHAR(50) NOT NULL UNIQUE
 )
