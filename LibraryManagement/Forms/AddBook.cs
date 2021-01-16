c70dbdddc1c66b43ed0677720293337c0393ff75
jonasminino <jonasminino@gmail.com>
2021-01-16 09:21:19 -0500

    Added a Add Book Button Click event.

--- LibraryManagement/Forms/AddBook.cs
+++ LibraryManagement/Forms/AddBook.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagement.Models;
+﻿using LibraryManagement.Helper;
+using LibraryManagement.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -30,13 +31,58 @@ namespace LibraryManagement.Forms
         /// Checks for missing needed information from the textboxes. 
         /// Checks if there is already a duplicate copy of the book.
         /// Adds a the new record if there is none.
-        /// Gives a choice to add a second copy to the records.
+        /// Gives a choice to add a second copy to the same record.
+        /// Displays a message that a new record or second copy has been added. 
+        /// Clears the fields after a new record is added.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+            //Checks if Title and Author fields are empty.//
+            if (txtTitle.Text == "" || txtAuthor.Text == "") MessageBox.Show("Are you mising the Title, or Author?", "Missing Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                Book book = new Book(null, txtTitle.Text, txtAuthor.Text, txtPublisher.Text, txtYear.Text.ToString(), txtISBN.Text, cbxType.Text, 1, 0);
+
+                //Checks if there is a record with the same Title, Author, and Publisher.//
+                if (AddBookHelper.ValidateBook(book))
+                {
+                    DialogResult result = MessageBox.Show("There is already a copy of a book with the same Title, Author, Publisher, and Year. Are you entering another copy?", "Duplicate Book", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes) if (AddBookHelper.AddCopy(AddBookHelper.GetBookId(book)) > 0)
+                        {
+                            DialogResult res = MessageBox.Show("You have added another copy of: " + txtTitle.Text + " by: " + txtAuthor.Text, "Successfuly Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            //Clears all fields after clicking ok.//
+                            if (res == DialogResult.OK)
+                            {
+                                txtAuthor.Clear();
+                                txtTitle.Clear();
+                                txtYear.Clear();
+                                txtPublisher.Clear();
+                                txtISBN.Clear();
+                                cbxType.SelectedIndex = -1;
+                            }                            
+                        }
+                }
+                else {//Message showing that a new record was added.//
+                    if (AddBookHelper.InsertBook(book) > 0) 
+                    {
+                        DialogResult result = MessageBox.Show("A new book record has been added.", "Record Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        
+                        //Clears all fields after clicking ok.//
+                        if(result == DialogResult.OK)
+                        {
+                            txtAuthor.Clear();
+                            txtTitle.Clear();
+                            txtYear.Clear();
+                            txtPublisher.Clear();
+                            txtISBN.Clear();
+                            cbxType.SelectedIndex = -1;
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- LibraryManagement/Models/Book.cs
+++ LibraryManagement/Models/Book.cs
@@ -8,17 +8,17 @@ namespace LibraryManagement.Models
 {
     public class Book
     {
-        private int? BookId { get; set; }
+        public Nullable<int> BookId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
         public string Year { get; set; }
-        public int ISBN { get; set; }
+        public string ISBN { get; set; }
         public string Type { get; set; }
         public int NumberOfCopies { get; set; }
         public int CheckedOut { get; set; }
 
-        public Book(int? id, string title, string author, string publisher, string year, int isbn, string type, int copies, int checkout)
+        public Book(Nullable<int> id, string title, string author, string publisher, string year, string isbn, string type, int copies, int checkout)
         {
             BookId = id;
             Title = title;
